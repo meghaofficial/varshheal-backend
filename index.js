b3f82ec11f24a045e1e5a8c8ad6a78de7edc2b7a
meghasharma <meghaofficial789@gmail.com>
2025-10-02 12:52:30 +0530

    added frontend url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-      origin: process.env.FRONTEND_URL,
+      origin: ['http://localhost:5173', process.env.FRONTEND_URL],
       credentials: true
 }));
 
